60d1b76855544971b10f7bed017e048639b892f3
denis blinkov <denis.blinkov@south.rt.ru>
2023-02-25 15:02:04 +0300

    add log in healthcheck

--- internal/loadbalancer/balancer.go
+++ internal/loadbalancer/balancer.go
@@ -1,60 +1,63 @@
 package loadbalancer
 
 import (
-	"github.com/denieryd/SimpleLoadBalancer/internal/backend"
-	"log"
-	"net/http"
-	"time"
+    "github.com/denieryd/SimpleLoadBalancer/internal/backend"
+    "log"
+    "net/http"
+    "time"
 )
 
 const (
-	Attempts int = iota
-	Retry
+    Attempts int = iota
+    Retry
 )
 
 var ServerPool backend.ServerPool
 
 func GetAttemptsFromContext(r *http.Request) int {
-	if attempts, ok := r.Context().Value(Attempts).(int); ok {
-		return attempts
-	}
+    if attempts, ok := r.Context().Value(Attempts).(int); ok {
+        return attempts
+    }
 
-	return 1
+    return 1
 }
 
 func GetRetryFromContext(r *http.Request) int {
-	if retry, ok := r.Context().Value(Retry).(int); ok {
-		return retry
-	}
-	return 0
+    if retry, ok := r.Context().Value(Retry).(int); ok {
+        return retry
+    }
+    return 0
 }
 
 func LoadBalance(w http.ResponseWriter, r *http.Request) {
-	attempts := GetAttemptsFromContext(r)
-	if attempts > 3 {
-		log.Printf("%s(%s) Max attempts reached, terminating\n", r.RemoteAddr, r.URL.Path)
-		http.Error(w, "Service not available", http.StatusServiceUnavailable)
-		return
-	}
+    attempts := GetAttemptsFromContext(r)
+    if attempts > 3 {
+        log.Printf("%s(%s) Max attempts reached, terminating\n", r.RemoteAddr, r.URL.Path)
+        http.Error(w, "Service not available", http.StatusServiceUnavailable)
+        return
+    }
 
-	peer := ServerPool.GetNextPeer()
-	if peer != nil {
-		peer.ReverseProxy.ServeHTTP(w, r)
-		return
-	}
+    peer := ServerPool.GetNextPeer()
+    if peer != nil {
+        peer.ReverseProxy.ServeHTTP(w, r)
+        return
+    }
 
-	http.Error(w, "Service not available", http.StatusServiceUnavailable)
+    http.Error(w, "Service not available", http.StatusServiceUnavailable)
 
 }
 
 func HealthCheck() {
-	t := time.NewTicker(time.Minute * 2)
-	for {
-		select {
-		case <-t.C:
-			log.Println("Start health checking")
-			ServerPool.HealthCheck()
-			log.Println("Health check completed")
-		}
-	}
+    timeToStart := time.Second * 30
+    log.Printf("first health check in %v\n", timeToStart)
+
+    t := time.NewTicker(timeToStart)
+    for {
+        select {
+        case <-t.C:
+            log.Println("Start health checking")
+            ServerPool.HealthCheck()
+            log.Println("Health check completed")
+        }
+    }
 }
